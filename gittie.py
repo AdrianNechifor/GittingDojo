19de90928cd131c86bdb396c524bd05dd4b3a197
AdrianNechifor <adriannechifor279@gmail.com>
2019-11-12 10:25:58 +0200

    ok

--- gittie.py
+++ gittie.py
@@ -6,11 +6,5 @@ def introduce():
 =======
 	print("Hello , I'm not Gittie!")
 >>>>>>> b343df37bf83585346510bc53581a3994416fba5
-<<<<<<< HEAD
 def add(a, b):
 	return a + b
-=======
-
-def joke():
-	print("This is a joke")
->>>>>>> 61b606888354a8ffbde80ba398d864bdceb8f257
