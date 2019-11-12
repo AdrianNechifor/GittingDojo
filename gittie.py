a83d5be4c0c0e9ca7a0b0c7cfc913cf4584c8147
AdrianNechifor <adriannechifor279@gmail.com>
2019-11-12 10:27:02 +0200

    resolved

--- gittie.py
+++ gittie.py
@@ -1,10 +1,7 @@
 haha
 
 def introduce():
-<<<<<<< HEAD
 	print("Hello , I'm Gittie!" * 3)
-=======
 	print("Hello , I'm not Gittie!")
->>>>>>> b343df37bf83585346510bc53581a3994416fba5
 def add(a, b):
 	return a + b
